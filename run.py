383d43716808a09ac9f1c189351e26326e9831a5
Rsrbai <rsrbailey@gmail.com>
2023-01-27 23:30:50 +0000

    Add class board and init method

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
+from random import randint
 
+class board:
+    def __init__(self, size, ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.ships = ships
+        self.name = name 
+        self.type = type
