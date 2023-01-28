c2b4f45e86d6d43425aaf274470b7b2b28023034
Rsrbai <rsrbailey@gmail.com>
2023-01-28 09:56:35 +0000

    Add more orintation options in place ship function

--- run.py
+++ run.py
@@ -7,3 +7,17 @@ class board:
         self.ships = ships
         self.name = name 
         self.type = type
+
+    def place_ship(self, ship, x, y, orientation):
+        if orientation == "up":
+            for i in range(ship.size):
+                self.board[x][y+i] = "S"
+        elif orientation == "down":
+            for i in range(ship.size):
+                self.board[x][y-i] = "S"
+        elif orientation == "left":
+            for i in range(ship.size):
+                self.board[x-i][y] = "S"
+        else:
+            for i in range(ship.size):
+                self.board[x+i][y]
